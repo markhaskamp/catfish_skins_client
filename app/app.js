3a68b1c41eb47bee8ff0ca949184b77e12d33453
Mark Haskamp <mark.haskamp@kroger.com>
2016-03-10 13:45:18 -0500

    refactor

--- app/app.js
+++ app/app.js
@@ -4,26 +4,37 @@ import { createStore } from 'redux';
 import $ from 'jquery';
 
 
-
-// components: enter my scores
+// components: form to enter my scores
 //             list of all scores
-// action types: add a score, update all scores
-// store: {[{golfer: 'john', scores: [0,4,3,4,4,...]},
-//          {golfer: 'tony', scores: [0,4,3,4,4,...]}]}
+// action types: add a score,
+//               update all scores
+// store: {counter: n,
+//         allScores: [{id: n, name: 'john', scores: [0,4,3,4,4,...]},
+//                     {id: n, name: 'tony', scores: [0,4,3,4,4,...]}
+//                    ]
+//        }
 
 
-function storeReducer(state, action) {
+let initialStore = {counter: 0,
+                    allScores: [{id:1, name: 'john', scores: [0,0,0,0,0,0,0,0,0,0]}
+                               ]
+                   };
+
+let store = createStore(storeReducer);
+
+function storeReducer(state=initialStore, action) {
+
     switch(action.type) {
     case 'add-score':
-        console.log(action.hole, action.strokes);
+        state.counter++;
 
-    case 'DECREMENT':
-        return state - 1;
+        return {counter: state.counter++,
+                allScores: state.allScores};
+
     default:
         return state;
     }
 }
-let store = createStore(storeReducer);
 
 export default class App extends React.Component {
     render() {
@@ -37,28 +48,49 @@ export default class App extends React.Component {
 
 export class MyScores extends React.Component {
 
+    constructor() {
+        super();
+
+        this.state = {counter: 0,
+                      scores: [0,0,0,0,0,0,0,0,0]};
+
+        store.subscribe(() => {
+            this.setState(store.getState())
+        })
+    }
+
     handleChangeHoleScore(e) {
       let hole = e.target.id;
       let selector = '#' + hole;
+      let action = {}
 
-      store.dispatch({type: 'add-score', hole: hole, strokes: $(selector).val()});
+      store.dispatch({type: 'add-score',
+                      "hole": hole,
+                      "strokes": $(selector).val()});
     }
 
     render() {
         return (
+          <div>
+            <div>{this.state.counter}</div>
             <table>
+              <thead>
               <tr>
                 <th>&nbsp;</th>
                 <th>1</th> <th>2</th> <th>3</th> <th>4</th> <th>5</th> <th>6</th> <th>7</th> <th>8</th> <th>9</th>
               </tr>
+              </thead>
 
+              <tbody>
               <tr>
                 <td>Mark</td>
                 <td><input id="1" onChange={this.handleChangeHoleScore.bind(this)}/></td>
                 <td><input id="2" onChange={this.handleChangeHoleScore.bind(this)}/></td>
                 <td><input id="3" onChange={this.handleChangeHoleScore.bind(this)}/></td>
               </tr>
+              </tbody>
             </table>
+          </div>
             )
     }
 }
