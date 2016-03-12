2110a64a95cb89f4ea77065f22170986788918d0
Mark Haskamp <mark.haskamp@pinnsg.com>
2016-03-12 16:11:13 -0500

    all 18 holes. app.css

--- app/app.js
+++ app/app.js
@@ -3,9 +3,12 @@ import { render} from "react-dom";
 import { createStore } from 'redux';
 import $ from 'jquery';
 
+import '../app/styles/app.css';
+
 
 // components: form to enter my scores
 //             list of all scores
+//               show one golfers scores
 // action types: add a score,
 //               update all scores
 // store: {counter: n,
@@ -74,19 +77,31 @@ export class MyScores extends React.Component {
           <div>
             <div>{this.state.counter}</div>
             <table>
-              <thead>
-              <tr>
-                <th>&nbsp;</th>
-                <th>1</th> <th>2</th> <th>3</th> <th>4</th> <th>5</th> <th>6</th> <th>7</th> <th>8</th> <th>9</th>
-              </tr>
-              </thead>
 
               <tbody>
               <tr>
-                <td>Mark</td>
-                <td><input id="1" onChange={this.handleChangeHoleScore.bind(this)}/></td>
-                <td><input id="2" onChange={this.handleChangeHoleScore.bind(this)}/></td>
-                <td><input id="3" onChange={this.handleChangeHoleScore.bind(this)}/></td>
+                <td><div className="nameLabel">Mark</div></td>
+                <td><input id="1" placeholder="1" onChange={this.handleChangeHoleScore.bind(this)}/></td>
+                <td><input id="2" placeholder="2" onChange={this.handleChangeHoleScore.bind(this)}/></td>
+                <td><input id="3" placeholder="3" onChange={this.handleChangeHoleScore.bind(this)}/></td>
+                <td><input id="4" placeholder="4" onChange={this.handleChangeHoleScore.bind(this)}/></td>
+                <td><input id="5" placeholder="5" onChange={this.handleChangeHoleScore.bind(this)}/></td>
+                <td><input id="6" placeholder="6" onChange={this.handleChangeHoleScore.bind(this)}/></td>
+                <td><input id="7" placeholder="7" onChange={this.handleChangeHoleScore.bind(this)}/></td>
+                <td><input id="8" placeholder="8" onChange={this.handleChangeHoleScore.bind(this)}/></td>
+                <td><input id="9" placeholder="9" onChange={this.handleChangeHoleScore.bind(this)}/></td>
+              </tr>
+              <tr>
+                <td><div className="nameLabel">&nbsp;</div></td>
+                <td><input id="10" placeholder="10" onChange={this.handleChangeHoleScore.bind(this)}/></td>
+                <td><input id="11" placeholder="11" onChange={this.handleChangeHoleScore.bind(this)}/></td>
+                <td><input id="12" placeholder="12" onChange={this.handleChangeHoleScore.bind(this)}/></td>
+                <td><input id="13" placeholder="13" onChange={this.handleChangeHoleScore.bind(this)}/></td>
+                <td><input id="14" placeholder="14" onChange={this.handleChangeHoleScore.bind(this)}/></td>
+                <td><input id="15" placeholder="15" onChange={this.handleChangeHoleScore.bind(this)}/></td>
+                <td><input id="16" placeholder="16" onChange={this.handleChangeHoleScore.bind(this)}/></td>
+                <td><input id="17" placeholder="17" onChange={this.handleChangeHoleScore.bind(this)}/></td>
+                <td><input id="18" placeholder="18" onChange={this.handleChangeHoleScore.bind(this)}/></td>
               </tr>
               </tbody>
             </table>
